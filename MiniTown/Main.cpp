45cef48336b2fa61f413865f98ab87229c15c786
XSXuan <55631422+XSXcser@users.noreply.github.com>
2021-01-08 12:17:44 +0800

    1

--- MiniTown/Main.cpp
+++ MiniTown/Main.cpp
@@ -13,7 +13,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
 #if defined(DEBUG) | defined(_DEBUG)
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 #endif
-
+//
 	GameApp theApp(hInstance);
 	
 	if( !theApp.Init() )
